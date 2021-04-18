fdcb80eb1bab1ed42e05ab6448f89c3b9ecc810c
Redwan Ahmed <redwanahmed1@hotmail.co.uk>
2021-04-18 01:56:42 +0100

    Added YouTube API and finished GET Request

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import youtube from '../apis/youtube'
 
 class App extends React.Component {
+    state = { videos: [] };
+
+// here we pass in the term which is being searched inside the search bar, then we call the youtube API and send a GET request
+    onTermSubmit = async term => {
+        const response = await youtube.get('/search', {
+            params: {
+                q: term
+            }
+        });
+        console.log(response.data.items);
+        this.setState({ videos: response.data.items });
+    };
+
     render () {
         return (
-        <div>
-            <SearchBar />
+        <div className="ui container">
+            <SearchBar onFormSubmit={this.onTermSubmit}/>
+            Videos: {this.state.videos.length}.
         </div>
         );
     }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,35 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    state = { term: '' };
+
+    // this function is an event listener which listens for change in the search bar
+    onInputChange = event => {
+        this.setState({ term: event.target.value });
+    };
+
+    onFormSubmit = (event) => {
+        // prevent the browser from automatically refreshing after user inputs
+        event.preventDefault();
+        // calling the props when user submits form
+        this.props.onFormSubmit(this.state.term);
+    };
+
     render() {
-        return <div>Search Bar</div>
+        return (
+            <div className="search-bar ui segment">
+                <form onSubmit={this.onFormSubmit} className="ui form">
+                    <div className="field">
+                        <label>YouTube Search</label>
+                        <input 
+                        type="text" 
+                        value={this.state.term}
+                        onChange={this.onInputChange}
+                        />
+                    </div>
+                </form>
+            </div>
+        );
     }
 }
 
